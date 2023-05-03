de35d3d0167c9242e4ad59919cb2a2ff3b1d8b4f
Eber <Eber.Oliveira@outlook.com.br>
2023-05-03 13:33:34 -0300

    Criado componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+function Login () {
+  return (
+    <ImageWithSpace>
+      Teste 12312312
+    </ImageWithSpace>
+  )
+}
+
+export default Login
