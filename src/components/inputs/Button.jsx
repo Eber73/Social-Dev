4422eb95128b5d80696c164ba55d4dba678f031c
Eber <Eber.Oliveira@outlook.com.br>
2023-05-05 11:54:34 -0300

    Criado componentes e tema

---
+++ src/components/inputs/Button.jsx
@@ -0,0 +1,17 @@
+ import styled from "styled-components"
+ 
+ const Button = styled.button`
+  background-color: ${props => props.theme.primary};
+  padding: 15px 20px;
+  border-radius: 10px;
+  border: none;
+  font-weight: bold;
+  color: ${props => props.theme.white};
+  transition: 0,3s; 
+
+  :hover {
+    background-color: ${props => props.theme.primaryHover};
+    }
+  `
+
+ export default Button
